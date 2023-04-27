4b6e7bd5c4f3b33cc5a6fb18d6dd80450fb986a0
SabrarLucas <pinchonlucas03@gmail.com>
2023-04-27 16:38:58 +0200

    page admin

---
+++ src/Controller/Admin/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/admin', 'admin_')]
+class MainController extends AbstractController
+{
+    #[Route('/', 'index')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig');
+    }
+}
