e103e9486529a7bf9078af7753367b4c914e235a
SabrarLucas <pinchonlucas03@gmail.com>
2023-05-03 15:38:10 +0200

    mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,11 +9,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
     {
         $contact = new Contact();
 
@@ -32,6 +34,16 @@ class ContactController extends AbstractController
 
             $manager->persist($contact);
             $manager->flush();
+
+            // Email
+            $email = (new Email())
+                ->from('hello@example.com')
+                ->to('you@example.com')
+                ->subject('Time for Symfony Mailer!')
+                ->text('Sending emails is fun again!')
+                ->html('<p>See Twig integration for better HTML integration!</p>');
+
+            $mailer->send($email);
 
             $this->addFlash(
                 'success',
