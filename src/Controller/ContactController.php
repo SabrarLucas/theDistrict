cf0c4837a0b2097583b8043aa9263de29258c5bf
SabrarLucas <pinchonlucas03@gmail.com>
2023-05-03 14:57:05 +0200

    formulaire contact quand connecter

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,10 +13,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $manager): Response
     {
         $contact = new Contact();
+
+        if ($this->getUser()) {
+            $contact->setNom($this->getUser()->getNom())
+                ->setPrenom($this->getUser()->getPrenom())
+                ->setEmail($this->getUser()->getEmail());
+        }
+
         $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            $manager->persist($contact);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre demande a été envoyé avec succès !'
+            );
+
+            return $this->redirectToRoute('contact');
+        }
 
         return $this->render('pages/contact/index.html.twig', [
             'form' => $form->createView()
